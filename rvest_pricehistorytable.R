f7a5e0c875a25b01a41bab08c33fd081415e11dd
dstephens <Ninj@8182>
2022-03-18 22:03:41 -0400

    Final updates on pulling and cleaning gold price & USD/MXN exchange rates.

--- rvest_pricehistorytable.R
+++ rvest_pricehistorytable.R
@@ -7,8 +7,14 @@ dates <- data.frame(seq(as.Date("2015-07-01"), Sys.Date()-1, by = "days"))
 names(dates)[1] <- 'date'
 
 
+### pull in historical MXN exchange rate data from csv, mutate date from chr to date, and change column name
+hist_mxn <- read_csv("C:\\Users\\dstephens\\Desktop\\R Project\\rvest\\csv\\2015-2021.csv") %>% 
+              mutate(., date = readr::parse_date(date, "%m/%d/%Y"))
+names(hist_mxn)[2] <- 'hist_mxn'
 
-### scrape gold price data -- USD per oz.
+
+
+### scrape all necessary gold price data -- USD per oz.
 scrape <- function(year){
   
   url <- paste("https://www.usagold.com/daily-gold-price-history/?ddYears=", year, sep = "")
@@ -32,42 +38,57 @@ gold_price <- gold_price[rev(order(gold_price$date)),] %>%
 
 
 
-# join dates and gold_price, and fill in NA data with previous days' data.
+# join dates and gold_price, fill in NA data with previous days' data.
 gold_price <- left_join(dates,
-                        gold_price %>% fill(`Closing Price`),
-                        by = "date")
+                        gold_price,
+                        by = "date") %>% 
+              fill(`Closing Price`) %>%
+              rename(gold_usd_oz = 'Closing Price')
 
 
 head(gold_price)
 
 
 
-### scrape USD/MXN data from 
+### scrape current USD/MXN data
 
 page <- "https://finance.yahoo.com/quote/USDMXN%3DX/history?period1=1070150400&period2=1647475200&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true"
 
-usd_mxn <- as.data.frame(
+current_mxn <- as.data.frame(
   read_html(page) %>%
   html_nodes("table") %>%
   html_table
   )
 
-
-usd_mxn <- mutate(usd_mxn, date = parse_date(Date, "%b %d, %Y"), mxn = parse_double(Close.))
-
-
+#parse_date to ISO, and mxn to double.
+current_mxn <- mutate(current_mxn, date = parse_date(Date, "%b %d, %Y"), mxn = parse_double(Close.))
 
 
-
-
-
-gold_mxn <- left_join(gold_price,
-                      usd_mxn %>% select(date, mxn),
-                      by = "date")
+head(current_mxn)
 
 
 
 
+### left_join historical MXN data with gold price (in USD per oz), and then left_join with current MXN data.
+gold_mxn <- left_join(gold_price, hist_mxn, by = 'date') %>%
+              left_join(., current_mxn %>% select(date, mxn), by = 'date')
 
 
+# if mxn column is NA, replace with hist_mxn, otherwise keep mxn.
+gold_mxn$mxn <- ifelse(is.na(gold_mxn$mxn), gold_mxn$hist_mxn, gold_mxn$mxn)
+
+
+# fill all NA's with previous date's price.
+gold_mxn <- gold_mxn %>% fill(mxn)
+
+
+# drop hist_msn column, as not needed.
+gold_mxn = select(gold_mxn, -hist_mxn) 
+
+
+
+
+
+### finally.
 View(gold_mxn)
+
